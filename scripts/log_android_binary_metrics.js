c71142e77af8c5ea5d96f6508c0b76dad03dd160
Nadia Barbosa <nadiabarbosa@me.com>
2019-01-08 10:54:36 -0800

    Write results to temp file to account for iOS and Android CI workflow timing differences

--- scripts/log_android_binary_metrics.js
+++ scripts/log_android_binary_metrics.js
@@ -14,7 +14,7 @@ const binaries = [
   ["Android", "x86_64", "platform/android/MapboxGLAndroidSDK/build/intermediates/intermediate-jars/release/jni/x86_64/libmapbox-gl.so"]
 ]
 
-const binaryMetricsPayload = binaries.map(binary => {
+const androidMetrics = binaries.map(binary => {
   return JSON.stringify({
       'sdk': 'maps',
       'platform' : binary[0],
@@ -22,21 +22,36 @@ const binaryMetricsPayload = binaries.map(binary => {
       'size' : fs.statSync(binary[2]).size,
       'created_at': `${date.getUTCFullYear()}-${date.getUTCMonth() + 1}-${date.getUTCDate()}`
   })
+}).join('\n') + '\n';
+
+fs.appendFile('temp-metrics.json', androidMetrics, (err) => {
+  if (err) throw err;
+  console.log('Android metrics written to temporary file');
 });
 
-var params = {
-    // Transform to line-delimited JSON (ndjson)
-    Body: zlib.gzipSync(binaryMetricsPayload.join('\n')),
-    Bucket: 'mapbox-loading-dock',
-    Key: `raw/nadia_staging_test_v2/${process.env['CIRCLE_SHA1']}.json.gz`,
-    CacheControl: 'max-age=300',
-    ContentType: 'application/json'
-};
+fs.readFile('temp-metrics.json', (err, data) => {
+  if (err) throw err;
 
-return new AWS.S3({region: 'us-east-1'}).putObject(params, function (err, res) {
-  if (err) {
-    console.log("Error sending Android binary metrics to S3: ", err);
-  } else {
-    console.log("Android binary size logged to S3 successfully")
-  }
+  var payload = data.toString('utf8').trim()
+
+  var params = {
+      Body: zlib.gzipSync(payload),
+      Bucket: 'mapbox-loading-dock',
+      Key: `raw/nadia_staging_test_v2/${process.env['CIRCLE_SHA1']}.json.gz`,
+      CacheControl: 'max-age=300',
+      ContentType: 'application/json'
+  };
+  
+  return new AWS.S3({region: 'us-east-1'}).putObject(params, function (err, res) {
+    if (err) {
+      console.log("Error sending Android binary metrics to S3: ", err);
+    } else {
+      console.log("Android binary size logged to S3 successfully")
+      
+      fs.unlink('temp-metrics.json', (err) => {
+        if (err) throw err;
+        console.log('temp-metrics.json was deleted');
+      });
+    }
+  });
 });
--- scripts/log_ios_binary_metrics.js
+++ scripts/log_ios_binary_metrics.js
@@ -13,7 +13,7 @@ const binaries = [
   ["iOS", "x86_64", "build/ios/pkg/dynamic/Mapbox-stripped-x86_64"]
 ]
 
-const binaryMetricsPayload = binaries.map(binary => {
+const iosMetrics = binaries.map(binary => {
   return JSON.stringify({
       'sdk': 'maps',
       'platform' : binary[0],
@@ -21,21 +21,36 @@ const binaryMetricsPayload = binaries.map(binary => {
       'size' : fs.statSync(binary[2]).size,
       'created_at': `${date.getUTCFullYear()}-${date.getUTCMonth() + 1}-${date.getUTCDate()}`
   })
+}).join('\n') + '\n';
+
+fs.appendFile('temp-metrics.json', iosMetrics, (err) => {
+  if (err) throw err;
+  console.log('iOS metrics written to temporary file');
 });
 
-var params = {
-    // Transform to line-delimited JSON (ndjson)
-    Body: zlib.gzipSync(binaryMetricsPayload.join('\n')),
-    Bucket: 'mapbox-loading-dock',
-    Key: `raw/nadia_staging_test_v2/${process.env['CIRCLE_SHA1']}.json.gz`,
-    CacheControl: 'max-age=300',
-    ContentType: 'application/json'
-};
+fs.readFile('temp-metrics.json', (err, data) => {
+  if (err) throw err;
 
-return new AWS.S3({region: 'us-east-1'}).putObject(params, function (err, res) {
-  if (err) {
-    console.log("Error sending iOS binary metrics to S3: ", err);
-  } else {
-    console.log("iOS binary size logged to S3 successfully")
-  }
+  var payload = data.toString('utf8').trim()
+
+  var params = {
+      Body: zlib.gzipSync(payload),
+      Bucket: 'mapbox-loading-dock',
+      Key: `raw/nadia_staging_test_v2/${process.env['CIRCLE_SHA1']}.json.gz`,
+      CacheControl: 'max-age=300',
+      ContentType: 'application/json'
+  };
+  
+  return new AWS.S3({region: 'us-east-1'}).putObject(params, function (err, res) {
+    if (err) {
+      console.log("Error sending iOS binary metrics to S3: ", err);
+    } else {
+      console.log("iOS binary size logged to S3 successfully")
+      
+      fs.unlink('temp-metrics.json', (err) => {
+        if (err) throw err;
+        console.log('temp-metrics.json was deleted');
+      });
+    }
+  });
 });
