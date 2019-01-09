5d38def92486076d182c1450a4c4451575cd1e2f
Nadia Barbosa <nadiabarbosa@me.com>
2019-01-09 10:15:42 -0800

    + ,

--- scripts/log_android_binary_metrics.js
+++ scripts/log_android_binary_metrics.js
@@ -17,7 +17,7 @@ const binaries = [
 const androidMetrics = binaries.map(binary => {
   return JSON.stringify({
       'sdk': 'maps',
-      'platform' : 'Android'
+      'platform' : 'Android',
       'arch': binary[0],
       'size' : fs.statSync(binary[1]).size,
       'created_at': `${date.getUTCFullYear()}-${date.getUTCMonth() + 1}-${date.getUTCDate()}`
